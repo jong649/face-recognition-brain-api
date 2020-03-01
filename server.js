6bb1d5f5f0815df697fbbbcd52b5b4b13c68495c
Jon <jong649@gmail.com>
2020-03-01 00:26:04 -0800

    adding Heroku database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require("./controllers/image.js");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-polished-17370",
     user: "jon",
     password: "test123",
     database: "smart-brain"
