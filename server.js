6c619ce28412dc837986ce7ecd57ef83b08376d4
Jon <jong649@gmail.com>
2020-03-01 00:28:53 -0800

    modify database options

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require("./controllers/image.js");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-polished-17370",
-    user: "jon",
-    password: "test123",
-    database: "smart-brain"
+    host: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
